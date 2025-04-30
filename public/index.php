935b43a9b83a2a3a8eb9a45f891f9c0394b3a300
Deivisito <henriquemaciasdavidalejandro@fpllefia.com>
2025-04-30 16:14:09 +0000

    Actu

--- public/index.php
+++ public/index.php
@@ -13,5 +13,12 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$response = $app->handle(
+    Request::capture()
+);
+
+$response->send();
+
+$app->terminate($request = Request::capture(), $response);
