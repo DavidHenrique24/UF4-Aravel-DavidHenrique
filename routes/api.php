d02de9ee568b824eca1bf245a5f8af24d02dafe3
Deivisito <henriquemaciasdavidalejandro@fpllefia.com>
2025-05-14 23:51:18 +0000

    Routas

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,40 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\StudentController;
+use App\Http\Controllers\AuthController;
+use App\Http\Middleware\isUserAuth;
+use App\Http\Middleware\isAdmin;
+use App\Models\User;
+use App\Http\Controllers\Api\TarjetaController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+
+Route::get('/tarjetas', [TarjetaController::class, 'index']);
+Route::post('/tarjetas', [TarjetaController::class, 'store']);
+Route::get('/tarjetas/{id}', [TarjetaController::class, 'show']);
+Route::put('/tarjetas/{id}', [TarjetaController::class, 'update']);
+Route::patch('/tarjetas/{id}', [TarjetaController::class, 'updatePartial']);
+Route::delete('/tarjetas/{id}', [TarjetaController::class, 'destroy']);
+
+
+
+
+
+
+//          --------------PRUEBAS -------------------
+
+// Route::get('/students', function () {
+//     return response('lista de estudiantessss', 200);
+// });
+
+//  Route::post('/students', function () { return 'Creating student'; });
+
+//  Route::put('/students/{id}', function () { return 'Updating student';
+// });
+
+//  Route::delete('/students/{id}', function () { return 'Deleting
+// student'; });
+
+// Route::get('/students/{id}', function () { return 'Getting single
+// student'; });
